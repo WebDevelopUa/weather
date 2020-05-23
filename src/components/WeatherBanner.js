f9ff357c1d898804c7e4961c0d09ce46b355053e
WebDev <39054829+WebDevelopUa@users.noreply.github.com>
2020-05-23 12:32:04 +0300

    Update WeatherBanner.js

--- src/components/WeatherBanner.js
+++ src/components/WeatherBanner.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const WeatherBanner = () => (
     <div id="weatherBanner" className="ui segment image">
-        <a href="https://github.com/WebDevelopUa/weather" target="_blank">
+        <a href="https://github.com/WebDevelopUa/weather" target="_blank" rel="noopener noreferrer">
             <img src="https://via.placeholder.com/468x60/009C95/FFFFFF?text=VISIT+APP+REPO+BANNER" alt="BANNER"/>
         </a>
     </div>
