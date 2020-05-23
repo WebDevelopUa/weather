85914d1c1a835e3d9a00f47274948e61f84f80d4
WebDevelopUa <js.develop.com@gmail.com>
2020-05-23 09:36:41 +0000

    Updates

---
+++ precache-manifest.005604eecc6f5cd1a67ec267557f61e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66976639165255e6d505e78145bdf099",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4c34db9c7e44a6d5f67c",
+    "url": "./static/css/main.7d1fb209.chunk.css"
+  },
+  {
+    "revision": "b1c5bf17fe64c96f3c54",
+    "url": "./static/js/2.a8706762.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.a8706762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c34db9c7e44a6d5f67c",
+    "url": "./static/js/main.12067471.chunk.js"
+  },
+  {
+    "revision": "0c4425f4c32fe3bf43fb",
+    "url": "./static/js/runtime-main.90a2a3b9.js"
+  },
+  {
+    "revision": "19461b5bfc69ee6c637d55f7f5932a4f",
+    "url": "./static/media/weather-logo.19461b5b.svg"
+  }
+]);
